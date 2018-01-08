cf0076bbdc2c55100cc2b2e780e11f2d9d7490b4
Maidi Khiter <maidi.khiter@hotmail.fr>
2018-01-08 16:57:54 +0100

    requête et recuperation de la liste des contacts dans MainActivity

--- app/src/main/java/mk/fr/localsqlapp/MainActivity.java
+++ app/src/main/java/mk/fr/localsqlapp/MainActivity.java
@@ -1,9 +1,17 @@
 package mk.fr.localsqlapp;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.mk.database.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
         Intent formIntent = new Intent(this, FormActivity.class);
         startActivity(formIntent);
 
+    }
+
+    private List<Map<String, String>> getAllContacts() {
+        //Instanciation de la connexion à la base de données
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        //Execution de la requête de selection
+        Cursor cursor = db.getReadableDatabase().rawQuery("SELECT name, first_name, email FROM contacts", null);
+
+        //Instanciation de la liste qui recevra les données
+        List<Map<String, String>> contactList=new ArrayList<>();
+        Map<String, String> contactCols = new HashMap<>();
+
+        //Parcours du curseur
+        while (cursor.moveToNext()){
+            contactCols.put("name", cursor.getString(0));
+            contactCols.put("first_name", cursor.getString(1));
+            contactCols.put("email", cursor.getString(2));
+
+            //Ajout du map à la list
+            contactList.add(contactCols);
+        }
+
+        return contactList;
     }
 }
